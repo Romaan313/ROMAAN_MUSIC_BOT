14c0caaca6aed3baa3bd72a35a18f0c8db0c98a6
𝙉𝙖𝙮𝙧𝙖 ✗ 𝙍𝙤𝙗𝙤𝙩 <168265371+NayraXRobot@users.noreply.github.com>
2024-06-16 18:40:24 +0530

    Update Ping.py

--- Ping.py
+++ Ping.py
@@ -70,10 +70,10 @@ async def ping_com(client, message: Message, _):
         ],
         [
             InlineKeyboardButton(
-                text="✦ ɢʀᴏᴜᴘ ✦", url=f"https://t.me/tmm_heroku_world",
+                text="✦ ɢʀᴏᴜᴘ ✦", url=f"https://t.me/AboutshayarAkhawab",
             ),
             InlineKeyboardButton(
-                text="✧ ᴍᴏʀᴇ ✧", url=f"https://t.me/tmm_support_chat",
+                text="✧ ᴍᴏʀᴇ ✧", url=f"https://t.me/shayariquote1",
             )
         ],
         [
