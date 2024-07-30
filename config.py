5acfb609047f2ce19d32f22f4cb9b33a7283befe
Romaan <108215028+Romaan313@users.noreply.github.com>
2024-07-30 10:13:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/NayraXRobot/cybershipahi",
+    "https://github.com/Romaan313/ROMAAN_MUSIC_BOT",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
